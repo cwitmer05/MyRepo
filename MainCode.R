34ce955a3c643cc6c853b17d4539acb6df8f3f08
cwitmer05 <cwitmer@purdue.edu>
2024-11-08 15:22:38 -0500

    Updated code

--- MainCode.R
+++ MainCode.R
@@ -3,38 +3,57 @@ install.packages("caret")
 install.packages("dplyr")
 install.packages("tidyr")
 install.packages("ggplot2")
+library(dplyr)
 library(readxl)
+library(tidyr)
+
+#Read and convert to dataframe
 sales_data <- read_excel("~/Desktop/SalesData.xlsx")
 sales_data <- as.data.frame(sales_data)
-#Missing Values
+
+#Missing Value Removal
 sum(is.na(sales_data))
-library(tidyr)
 sales_data <- drop_na(sales_data)
 
-library(dplyr)
-#Outliers~Sales
+#OutlierRemoval: Sales
 SQ1 <- quantile(sales_data$Sales, 0.25)
 SQ3 <- quantile(sales_data$Sales, 0.75)
 SIQR <- SQ3 - SQ1
-Slower_bound <- SQ1 - 1.5 * IQR
-Supper_bound <- SQ3 + 1.5 * IQR
+sales_lower_bound <- SQ1 - 1.5 * SIQR
+sales_upper_bound <- SQ3 + 1.5 * SIQR
 sales_data <- sales_data %>%
-  filter(Sales>= Slower_bound & Sales<= Supper_bound)
+  filter(Sales>= sales_lower_bound & Sales<= sales_upper_bound)
 
-#Outliers~Quantity
+#OutlierRemoval: Quantity
 QQ1 <- quantile(sales_data$Quantity, 0.25)
 QQ3 <- quantile(sales_data$Quantity, 0.75)
 QIQR <- QQ3 - QQ1
-Qlower_bound <- QQ1 - 1.5 * QIQR
-Qupper_bound <- QQ3 + 1.5 * QIQR
+quantity_lower_bound <- QQ1 - 1.5 * QIQR
+quantity_upper_bound <- QQ3 + 1.5 * QIQR
 sales_data <- sales_data %>%
-  filter(Quantity>= Qlower_bound & Quantity<= Qupper_bound)
+filter(Quantity>= quantity_lower_bound & Quantity<= quantity_upper_bound)
 
-#Outliers~Discount
+#OutlierRemoval: Discount
 DQ1 <- quantile(sales_data$Discount, 0.25)
 DQ3 <- quantile(sales_data$Discount, 0.75)
 DIQR <- DQ3 - DQ1
-Dlower_bound <- DQ1 - 1.5 * DIQR
-Dupper_bound <- DQ3 + 1.5 * DIQR
+discount_lower_bound <- DQ1 - 1.5 * DIQR
+discount_upper_bound <- DQ3 + 1.5 * DIQR
 sales_data <- sales_data %>%
-  filter(Discount>= Dlower_bound & Discount<= Dupper_bound)
+filter(Discount>= discount_lower_bound & Discount<= discount_upper_bound)
+
+#Duplicate Removal
+sales_data <- sales_data %>% distinct()
+
+#Inconsistency Check (less of a worry since they are all numeric)
+unique(sales_data$Sales)
+unique(sales_data$Quantity)
+unique(sales_data$Discount)
+
+#Creation of Profit Margin Column (Target/Outcome variable of model)
+sales_data$profit_margin <- sales_data$Profit / sales_data$Sales
+
+#Cleaned Table with only important variables
+sales_data <- sales_data %>%
+  select(Sales, Quantity, Discount, profit_margin)
+head(sales_data, 20)
