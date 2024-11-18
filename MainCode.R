a3773bec22676e1e2477e89aa99f4a4d9457542a
cwitmer05 <cwitmer@purdue.edu>
2024-11-18 15:34:09 -0500

    Update

--- MainCode.R
+++ MainCode.R
@@ -148,3 +148,6 @@ summary(sales_data$Sales)
 summary(sales_data$Quantity)
 summary(sales_data$Discount)
 summary(sales_data$profit_margin)
+
+model1 <- lm(profit_margin ~ Sales + Quantity + Discount, data = sales_data)
+summary(model1)
